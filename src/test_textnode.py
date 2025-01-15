3b25877a1c7165252150556d81936aeae3d2112e
JR <jrpatton1234@gmail.com>
2025-01-15 14:02:22 -0500

    Add test for textnode

---
+++ src/test_textnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertIsNone(node.url)
+
+    def test_neq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node 1", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_neq_text_type(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_neq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://url.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://url_not.com")
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
